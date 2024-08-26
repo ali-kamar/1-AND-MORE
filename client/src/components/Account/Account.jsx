2a8ce2b10fdae348e94126a2d3f6fd314d7d7a4e
Ali Kamar <alykamarr@gmail.com>
2024-08-26 14:36:33 +0300

    styling

--- client/src/components/Account/Account.jsx
+++ client/src/components/Account/Account.jsx
@@ -41,7 +41,7 @@ const Account = () => {
            </button>
            <button
              type="submit"
-             className="mt-2 block w-full py-2 text-center text-white bg-primary border border-primary rounded hover:bg-transparent hover:text-primary transition uppercase font-roboto font-medium"
+             className="mt-2 block w-full py-2 text-center text-white bg-black border hover:border-primary rounded hover:bg-transparent hover:text-primary transition uppercase font-roboto font-medium"
            >
              Logout
            </button>
--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -4,7 +4,16 @@ import banner from "../../assets/images/banner.JPG";
 const Banner = () => {
   return (
     <div className="w-full xs:mt-8 sm:mt-0">
-      <img src={banner} alt="banner" className="w-full" />
+      <img src={banner} alt="banner" className="w-full xs:h-52 sm:h-auto" />
+      <div class="mt-12 flex justify-center">
+        <a
+          href="/shop"
+          class="bg-primary border border-primary text-white px-8 py-3 font-medium 
+                    rounded-md hover:bg-transparent hover:text-primary"
+        >
+          Shop Now
+        </a>
+      </div>
     </div>
   );
 };
