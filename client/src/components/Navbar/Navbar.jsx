eb8e834c95a09e99497f27679f5b5f0b012ea615
Ali Kamar <alykamarr@gmail.com>
2024-08-29 16:36:25 +0300

    fix user navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,13 +3,10 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className="bg-primary">
-      <div className="container flex">
-        <div className="flex items-center justify-between flex-grow md:pl-12 py-5">
-          <div className="flex items-center space-x-6 capitalize">
-            <a
-              href="/"
-              className="text-gray-200 hover:text-white transition"
-            >
+      <div className="sm:container flex">
+        <div className="flex items-center justify-between flex-grow xs:px-5 sm:px-0 md:pl-12 py-5">
+          <div className="flex items-center space-x-6 capitalize xs:text-16 lg:text-xl">
+            <a href="/" className="text-gray-200 hover:text-white transition">
               Home
             </a>
             <a
@@ -19,13 +16,16 @@ const Navbar = () => {
               Shop
             </a>
 
-            <a href="/orders" className="text-gray-200 hover:text-white transition">
+            <a
+              href="/orders"
+              className="text-gray-200 hover:text-white transition"
+            >
               Orders
             </a>
           </div>
           <a
             href="/contact"
-            className="text-gray-200 hover:text-white transition"
+            className="text-gray-200 hover:text-white transition xs:text-16 lg:text-xl"
           >
             Contact
           </a>
