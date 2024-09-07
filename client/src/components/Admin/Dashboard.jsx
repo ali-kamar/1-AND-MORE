5a3cb3f927ce0842d0a516dcc25fdc786ac7cf02
Ali Kamar <alykamarr@gmail.com>
2024-09-07 17:02:01 +0300

    apply dashboard product fetch

--- client/src/components/Admin/Dashboard.jsx
+++ client/src/components/Admin/Dashboard.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Searchbar from "../Products/Searchbar";
 import ProductsDashboard from "./ProductsDashboard";
+import { useProduct } from "../../contexts/Product/ProductProvider";
 
 const Dashboard = () => {
+  const { products } = useProduct();
   return (
     <div className="p-3">
       <h2 className="text-4xl font-semibold text-center my-10">Dashboard</h2>
@@ -15,15 +17,27 @@ const Dashboard = () => {
                 <th className="text-left px-4 py-2 border border-black">
                   Image
                 </th>
-                <th className="text-center px-4 py-2 border border-black">Name</th>
-                <th className="text-center px-4 py-2 border border-black">Description</th>
-                <th className="text-center px-4 py-2 border border-black">Price</th>
-                <th className="text-center px-4 py-2 border border-black">Offer(%)</th>
-                <th className="text-center px-4 py-2 border border-black">Actions</th>
+                <th className="text-center px-4 py-2 border border-black">
+                  Name
+                </th>
+                <th className="text-center px-4 py-2 border border-black">
+                  Description
+                </th>
+                <th className="text-center px-4 py-2 border border-black">
+                  Price
+                </th>
+                <th className="text-center px-4 py-2 border border-black">
+                  Offer(%)
+                </th>
+                <th className="text-center px-4 py-2 border border-black">
+                  Actions
+                </th>
               </tr>
             </thead>
             <tbody>
-              <ProductsDashboard />
+              {products.map((product) => (
+                <ProductsDashboard product={product} key={product.product_id} />
+              ))}
             </tbody>
           </table>
         </div>
--- client/src/components/Admin/ProductsDashboard.jsx
+++ client/src/components/Admin/ProductsDashboard.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { FaTrashAlt } from "react-icons/fa";
 import { AiOutlineEdit } from "react-icons/ai";
-import Logo from "../../assets/images/logo.JPG"
 
-const ProductsDashboard = () => {
+
+const ProductsDashboard = ({product}) => {
   return (
     <tr className="">
       <td className="py-4 px-4 min-w-[200px] border border-black">
         <div className="flex items-center gap-2">
           <img
-            src={Logo}
+            src={product.imageurl}
             alt="Product"
             className="w-32 h-32 object-cover" /* Adjust the size for smaller screens */
           />
@@ -17,19 +17,19 @@ const ProductsDashboard = () => {
       </td>
 
       <td className="font-semibold py-4 px-4 text-center min-w-[120px] border border-black">
-        <p>Name</p>
+        <p>{product.name}</p>
       </td>
 
       <td className="py-4 px-4 text-center min-w-[120px] border border-black">
-        <span className="text-black font-semibold">Description</span>
+        <span className="text-black font-semibold">{product.description}</span>
       </td>
 
       <td className="py-4 px-4 text-center min-w-[120px] border border-black">
-        <span className="text-black font-semibold">$price</span>
+        <span className="text-black font-semibold">${product.price}</span>
       </td>
 
       <td className="py-4 px-4 text-center min-w-[120px] border border-black">
-        <span className="text-black font-semibold">offer</span>
+        <span className="text-black font-semibold">{product.offertype}</span>
       </td>
 
       <td className="py-4 px-4 text-center min-w-[80px] border border-black">
