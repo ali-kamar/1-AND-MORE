cc205428bdfd92f7fee3773fd84eb6c7888e799e
Ali Kamar <alykamarr@gmail.com>
2025-05-05 14:43:35 +0300

    live

--- server/app.js
+++ server/app.js
@@ -26,12 +26,12 @@ app.use(express.json());
 
 // extra packages
 app.set("trust proxy", 1);
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
-);
+// app.use(
+//   rateLimiter({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100, // limit each IP to 100 requests per windowMs
+//   })
+// );
 app.use(express.json());
 app.use(helmet());
 app.use(cors(corsOptions));
