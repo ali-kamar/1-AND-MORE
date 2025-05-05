7bac38706c05a95b0dbebc827b68561c08a75cf1
Ali Kamar <alykamarr@gmail.com>
2025-05-05 14:06:30 +0300

    live

--- server/app.js
+++ server/app.js
@@ -32,7 +32,23 @@ app.use(
 );
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+const allowedOrigins = [
+  "https://1-and-more.vercel.app",
+  "https://1-and-more-git-main-alis-projects-53e71da3.vercel.app/",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(xss());
 // routes
